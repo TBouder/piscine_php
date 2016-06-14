5674ea9883463a4b50ef39978d104474ded3b7ad
Tbouder <tbouder@protonmail.com>
2016-06-14 12:51:24 +0200

    Auto-Update 12:51:24, Tuesday 14 June

--- d01/ex03/MAIN_TO_DEL.php
+++ d01/ex03/MAIN_TO_DEL.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?php
 	include("ft_split.php");
+	
 	print_r(ft_split("Helloo	World AAA"));
 	print_r(ft_split(" Helloo	World AAA"));
 	print_r(ft_split("	Helloo	World AAA"));
@@ -13,11 +14,4 @@
 	print_r(ft_split(""));
 	print_r(ft_split(FALSE));
 	print_r(ft_split("0 0 0 0 1 0 0 0 0"));
-	print_r(ft_split("Hello	World AAA"));
-	print_r(ft_split(""));
-	print_r(ft_split(" H e l l o"));
-	print_r(ft_split("ZZZ ZZA ZAZ "));
-	print_r(ft_split("0 1 2 3 4 5"));
-	print_r(ft_split("0 	 zero NULL 	 	EMPTY FALSE"));
-	print_r(ft_split(NULL));
 ?>
